f807590e4a36729af5daccd804ea4a415400c0a3
sardylan <sardylan@gmail.com>
2017-11-21 15:44:37 +0100

    Version 1.6.0

--- attivita/__openerp__.py
+++ attivita/__openerp__.py
@@ -3,39 +3,33 @@
 # this module contains the full copyright notices and license terms.
 
 {
-    'name': 'Attività',
-    'version': '8.0.1.5.0',
-    'category': 'Process',
-    'summary': 'Gestione Attività',
-    'author': 'Flosslab',
-    'website': 'http://www.flosslab.com.',
-    'license': 'AGPL-3',
-    'css': [
+    "name": "Attività",
+    "version": "8.0.1.6.0",
+    "category": "Process",
+    "summary": "Gestione Attività",
+    "author": "Flosslab",
+    "website": "http://www.flosslab.com",
+    "license": "AGPL-3",
+    "css": [
         "static/src/css/style.css",
     ],
     "depends": [
-        'base_setup',
-        'board',
-        'mail',
-        'resource',
-        'web_kanban',
-        'hr',
+        "base_setup",
+        "board",
+        "mail",
+        "resource",
+        "web_kanban",
+        "hr",
     ],
     "data": [
-        'security/account_security.xml',
-        'security/ir.model.access.csv',
-        'wizard/assegna_attivita_view.xml',
-        'wizard/wizard_attivita_view.xml',
-        'wizard/registra_aggiornamento_view.xml',
-        'view/attivita_view.xml',
-        'data/email_notifications.xml'
+        "security/account_security.xml",
+        "security/ir.model.access.csv",
+        "wizard/assegna_attivita_view.xml",
+        "wizard/wizard_attivita_view.xml",
+        "wizard/registra_aggiornamento_view.xml",
+        "view/attivita_view.xml",
+        "data/email_notifications.xml"
     ],
-    "images": [],
-    "update_xml": [],
-
-    "demo": [],
     "application": True,
-    "installable": True,
+    "installable": True
 }
-#
-##############################################################################
--- seedoo_attivita/__openerp__.py
+++ seedoo_attivita/__openerp__.py
@@ -2,32 +2,31 @@
 # This file is part of Seedoo.  The COPYRIGHT file at the top level of
 # this module contains the full copyright notices and license terms.
 
-{ 'name': 'Seedoo Attività',
-    'version': '8.0.1.5.1',
-    'category': 'Process',
-    'summary': 'Gestione Attività Seedoo',
-    'author': 'Flosslab',
-    'website': 'http://www.flosslab.com.',
-    'license': 'AGPL-3',
+{
+    "name": "Seedoo Attività",
+    "version": "8.0.1.6.0",
+    "category": "Process",
+    "summary": "Gestione Attività Seedoo",
+    "author": "Flosslab",
+    "website": "http://www.flosslab.com",
+    "license": "AGPL-3",
     "depends": [
-        'seedoo_protocollo', 'attivita', 'seedoo_gedoc'
+        "seedoo_protocollo",
+        "attivita",
+        "seedoo_gedoc"
     ],
     "data": [
-        'wizard/richiedi_classificazione_protocollo_wizard_view.xml',
-        'wizard/richiedi_annullamento_protocollo_wizard_view.xml',
-        'wizard/richiedi_protocollazione_wizard_view.xml',
-        'wizard/richiedi_fascicolazione_protocollo_wizard_view.xml',
-        'wizard/crea_attivita_protocollo_wizard_view.xml',
-        'view/seedoo_attivita_view.xml',
-        'view/gedoc_view.xml',
-        'data/seedoo_attivita_data.xml',
-        'security/ir.model.access.csv',
+        "wizard/richiedi_classificazione_protocollo_wizard_view.xml",
+        "wizard/richiedi_annullamento_protocollo_wizard_view.xml",
+        "wizard/richiedi_protocollazione_wizard_view.xml",
+        "wizard/richiedi_fascicolazione_protocollo_wizard_view.xml",
+        "wizard/crea_attivita_protocollo_wizard_view.xml",
+        "view/seedoo_attivita_view.xml",
+        "view/gedoc_view.xml",
+        "data/seedoo_attivita_data.xml",
+        "security/ir.model.access.csv",
     ],
-    "js": [],
-    "css": [],
-    "qweb": [],
-    "demo": [],
     "installable": True,
     "application": True,
-    "auto_install": False,
+    "auto_install": False
 }
